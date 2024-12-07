405867e81a399e3cbe2eb134d10c8f29e8a9e969
Faraz <faraz@Farazs-MacBook-Air.local>
2024-12-07 17:44:13 +0530

    day 4 completed

---
+++ band1.py
@@ -0,0 +1,12 @@
+# print(2 + 2)
+# print("hello python");
+# print ("hello \n ('Next')")
+
+# name = input("enter the object type");
+# print(len(name))
+
+print("welocme to Band name generator");
+city = input("whats the city you greq up?:\n");
+pet = input("whats your pet Name?\n");
+print("Your band name Could be " + city +" " + pet )
+
